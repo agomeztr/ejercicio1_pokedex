8f1c56cb6b55443084fb0b40e64b0da75c6ec3bd
agomeztr <agomeztr@emeal.nttdata.com>
2026-04-22 15:48:26 +0200

    Add theme to the page, configure the main page and print the pokemon image

--- web/modules/custom/pokedex/js/pokedex.js
+++ web/modules/custom/pokedex/js/pokedex.js
@@ -1,20 +1,26 @@
-//function to get a specific pokemon
-async function getPokemom(id) {
-    //call to api
-    const response = await fetch(`https://pokeapi.co/api/v2/${id}/`)
-    //when we got the response, we save it in const data
-    const data = await response.json()
-    //we return const data
-    return data
-}
+document.addEventListener("DOMContentLoaded", () => {
+    //create an array to save all the pokemons
+    let listPokemons = [];
+    //create an index to navigate between pokemons
+    let index = 0;
 
-async function init() {
-    //we get the fist pokemon
-    const fistPokemon = await getPokemom(150);
-    //print the name of pokemon
-    window.pokemon.textContent = fistPokemon.name
-    //print the pokemon image
-    window.image.setAttribute('src', fistPokemon.sprites.front_default)
-}
+    //call to the api
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+    .then(response => response.json())
+    .then(data => {
+        //save the data results (result in json api)
+        listPokemons = data.results;
+        console.log(data.results);
+        //show the pokemon
+        showPokemon();
+    })
 
-init()
+    //get the image from main page
+    let photo = document.getElementById("photo-pokemon");
+
+    //function to show the pokemon in the pokedex
+    function showPokemon(){
+        const pokemon = listPokemons[index];
+        photo.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + (index + 1) + ".png";
+    }
+})
--- web/modules/custom/pokedex/src/Controller/PokedexController.php
+++ web/modules/custom/pokedex/src/Controller/PokedexController.php
@@ -4,9 +4,40 @@ use Drupal\Core\Controller\ControllerBase;
 
 class PokedexController extends ControllerBase {
   public function content() {
-    $newVariable = "Hola mundo";
+    //Create the content elements
     return [
-      '#markup' => $newVariable,
+      '#markup' => '
+        <div id="pokedex">
+            <img id = "photo-pokedex" src= "/sites/default/files/2026-04/plantilla_pokedex.png">
+            <img id="photo-pokemon">
+            <button id"show-details">Detalles</button>
+            <h2 id="name-pokemon"><h2>
+            <section id="details">
+                <h4>Detalles</h4>
+                <span>
+                    <h6>Nombre: </h6>
+                    <p id="name"></p>
+                </span>
+                <span>
+                    <h6>Tipos: </h6>
+                    <ul id="types"></ul>
+                </span>
+                <span>
+                    <h6>Altura: </h6>
+                    <p id="height"></p>
+                </span>
+                <span>
+                    <h6>Peso: </h6>
+                    <p id="weight"></p>
+                </span>
+            </section>
+            <button class="previous">Previous</button>
+            <button class="next">Next</button>
+        </div>
+        ',
+        '#attached' => [
+            'library' => 'pokedex/pokedex',
+        ],
     ];
   }
 }
